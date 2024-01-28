e3d6b29b9ea53591fa272c4145b406235c8aa450
Mihai <dimamihaimail@gmail.com>
2024-01-28 18:09:56 +0200

    First commit

---
+++ src/main/java/com/mal/eventui/EventuiApplication.java
@@ -0,0 +1,13 @@
+package com.mal.eventui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventuiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mal/eventui/EventuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mal.eventui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventuiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
